6b93ccf209a518bb795751f445da012938ed5a50
Marqus Shoels <mshoels07@gmail.com>
2021-08-24 14:18:11 -0500

    settings.py updates

--- Shoels_Marqus_Python_Belt_Exam/settings.py
+++ Shoels_Marqus_Python_Belt_Exam/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3skpwp2can69a(xwvrwot2cbz6stdp3)ral3rhn^$q=09q#98'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
